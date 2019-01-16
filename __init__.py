bd12d1acbbd52687520b107227f59c08b5b2352a
husseintaleb <46750845+husseintaleb@users.noreply.github.com>
2019-01-16 15:32:06 +0100

    create class

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
 print("coucou")
 
 import requests
+import json
+
+res = requests.get("https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=3")
+
+obj = json.loads(res.text)
+
+class Dataset
